5de4de0b36050840a8ba2e19d23ed043a7131024
shankarsharma8089 <126678340+shankarsharma8089@users.noreply.github.com>
2023-06-02 23:38:53 +0530

    Bag Of Words.py

--- Bag Of Words.py
+++ Bag Of Words.py
@@ -45,4 +45,38 @@ for i in range (len(sentence)):
 #-Bow model, here we are using sklearn lib and importing lib called CountVectorizer which creates histogram or the BOW documents matrix.
 #-Then we create an object for this lib.
 #-Fit transform creates an matrix and .array() convert corpus to array.
+
+
+#simple code to understand the creation of dict of words and their occurance in BOW
+sent1 = "Kanye West is an American rapper, singer, and songwriter. He was born on February 22, 1977, in Brooklyn, New York. Kanye is known for his unique style of music, which often features a catchy melody and lyrics that are often inspired by pop culture and current events." 
+sent2 = "kanye has released several successful albums, including 808s & Heartbreak,The Life of Pablo among others. Kanye is also known for his social media presence,Kanye is known for his powerful message and his ability to connect with his audience. "
+sent3 = "He has often used his music to inspire others and has made a positive impact on people's lives. His message of love and acceptance has inspired countless artists and musicians around the world.Overall, Kanye West is a multi-faceted artist who has made a significant impact on the music industry and the world of entertainment. His success and popularity have inspired countless artists and musicians around the world."
+
+BOW1 = sent1.split(' ')
+BOW2 = sent2.split(' ')
+BOW3 = sent3.split(' ')
+print(BOW1)
+print(BOW2)
+print(BOW3)
+uniqewords = set(BOW1).union(set(BOW2)).union(set(BOW3))
+#print(uniqewords)
+
+#creating a dict of words and their occurace for each doc in the corpus (collection of documents)
+num_of_words_in_BOW1 = dict.fromkeys(uniqewords,0)
+for word in BOW1:
+    num_of_words_in_BOW1[word] +=1
+
+num_of_words_in_BOW2 = dict.fromkeys(uniqewords,0)
+for word in BOW2:
+    num_of_words_in_BOW1[word] +=1    
+    
+num_of_words_in_BOW3 = dict.fromkeys(uniqewords,0)
+for word in BOW3:
+    num_of_words_in_BOW1[word] +=1  
+
+
+print(num_of_words_in_BOW1)
+print(num_of_words_in_BOW2)
+print(num_of_words_in_BOW3)
+
     
