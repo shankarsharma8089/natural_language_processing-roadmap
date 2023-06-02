dd849d8112f7119cefbece8467a392bc4a7875c2
shankarsharma8089 <126678340+shankarsharma8089@users.noreply.github.com>
2023-06-02 18:44:08 +0530

    Tokenization.py

--- Tokenization.py
+++ Tokenization.py
@@ -7,8 +7,8 @@ nltk.download('punkt')
 
 paragraph = "Kanye West is an American rapper, singer, and songwriter. He was born on February 22, 1977, in Brooklyn, New York. Kanye is known for his unique style of music, which often features a catchy melody and lyrics that are often inspired by pop culture and current events. He has released several successful albums, including 808s & Heartbreak,The Life of Pabloand West Side Story, among others. Kanye is also known for his social media presence,Kanye is known for his powerful message and his ability to connect with his audience. He has often used his music to inspire others and has made a positive impact on people's lives. His message of love and acceptance has inspired countless artists and musicians around the world.Overall, Kanye West is a multi-faceted artist who has made a significant impact on the music industry and the world of entertainment. His success and popularity have inspired countless artists and musicians around the world."
 
-#sentence = nltk.sent_tokenize(paragraph)
-#print(sentence)
+sentence = nltk.sent_tokenize(paragraph)
+print(sentence)
 
 ### 1.2)*Tokenizing words*
 
