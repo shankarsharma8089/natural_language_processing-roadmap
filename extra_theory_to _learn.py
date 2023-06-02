1114f52e6240bcda80a15dc18028a34ce52276b7
shankarsharma8089 <126678340+shankarsharma8089@users.noreply.github.com>
2023-06-02 23:53:57 +0530

    Update extra_theory_to _learn.py

--- extra_theory_to _learn.py
+++ extra_theory_to _learn.py
@@ -22,3 +22,4 @@
 #-In CBOW we try to predict the center word from the context word
 #-As the number size is 3 and the center word is the word we need to predict using the 2 word surrounding it, to do this we use a simple NN(Neural Network)
 #-Here we will put ‘Liverpool’ and ‘are’ as input to NN to find the middle word ‘FC’.
+![WhatsApp Image 2022-09-02 at 9 23 20 AM](https://github.com/shankarsharma8089/natural_language_processing-roadmap/assets/126678340/5611c966-5d12-45e4-a3ca-6eddd69d7725)
